bcf641833e9e550b638c2950e36a4c18e2db5473
Jason Coley <jason@software-solutions.co.nz>
2023-02-28 12:00:17 +1000

    Initial commit

---
+++ setup/src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+	return (
+		<section className="section about-section">
+			<h1 className="section-title">about us</h1>
+			<p>
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero,
+				necessitatibus facilis harum qui assumenda repellat illo vel doloremque
+				iure quam perferendis praesentium fuga corrupti voluptatum aut culpa
+				itaque fugiat eligendi quidem? Quam modi, quia dolorum debitis dolor et
+				corporis, aut sit temporibus neque consequuntur?
+			</p>
+		</section>
+	);
+};
+
+export default About;
