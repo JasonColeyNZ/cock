de4b51a7552a6284767287b1a961fb605654e94b
Jason Coley <jason@software-solutions.co.nz>
2023-02-28 12:10:43 +1000

    Changed About back

--- setup/src/pages/About.js
+++ setup/src/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
 		<section className="section about-section">
 			<h1 className="section-title">about us</h1>
 			<p>
-				Test Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero,
+				Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero,
 				necessitatibus facilis harum qui assumenda repellat illo vel doloremque
 				iure quam perferendis praesentium fuga corrupti voluptatum aut culpa
 				itaque fugiat eligendi quidem? Quam modi, quia dolorum debitis dolor et
